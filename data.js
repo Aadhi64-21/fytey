91f93ad7a8c6477f6887977bc8639d783a05c074
Aadhi64-21 <alekshmi222@gmail.com>
2025-07-17 09:16:41 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama",
+      "name": "Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.2644629620683716,
+        "pitch": -0.0016357071768222653,
+        "fov": 1.3726308726687002
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
